dbd857c2409943d3d32bd607aa4a4e350f79d328
Min Kim <msk@dolbo.net>
2019-07-28 14:24:03 -0700

    Add a randomized test for BallTree::query()

--- src/ball_tree.rs
+++ src/ball_tree.rs
@@ -398,7 +398,8 @@ fn max_spread_column(matrix: &ArrayView2<f64>, idx: &[usize]) -> usize {
 #[cfg(test)]
 mod test {
     use super::*;
-    use ndarray::{aview1, aview2, Axis};
+    use ndarray::{aview1, aview2, ArrayView, Axis};
+    use rand::prelude::*;
 
     #[test]
     #[should_panic]
@@ -490,6 +491,25 @@ mod test {
         let point = aview1(&[1., 2.]);
         let neighbor = tree.query_one(&point);
         assert_eq!(neighbor.distance, 1f64);
+    }
+
+    #[test]
+    fn ball_tree_query() {
+        const DIMENSION: usize = 3;
+
+        let mut rng = rand::thread_rng();
+        let data: Vec<f64> = (0..40 * DIMENSION).map(|_| rng.gen()).collect();
+        let array = ArrayView::from_shape((40, DIMENSION), &data).unwrap();
+        let bt = BallTree::new(array.clone());
+        for _ in 0..10 {
+            let query: Vec<f64> = (0..DIMENSION).map(|_| rng.gen()).collect();
+            let bt_neighbors = bt.query(&aview1(&query), 5);
+            let naive_neighbors =
+                naive_k_nearest_neighbors(&array, &aview1(&query), 5, euclidean_distance);
+            for (n_bt, n_naive) in bt_neighbors.iter().zip(naive_neighbors.iter()) {
+                assert_eq!(n_bt.distance, n_naive.distance);
+            }
+        }
     }
 
     #[test]
