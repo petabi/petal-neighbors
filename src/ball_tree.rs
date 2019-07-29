1c25ea4f3ee1de2d65d1e93e3ef383bd77c621d2
Min Kim <msk@dolbo.net>
2019-07-28 20:07:01 -0700

    Add a randomized test for BallTree::query() (#15)
    * Add a native KNN function for test
    
    * Add a randomized test for BallTree::query()

--- src/ball_tree.rs
+++ src/ball_tree.rs
@@ -193,7 +193,7 @@ impl<'a> BallTree<'a> {
     }
 }
 
-#[derive(Debug)]
+#[derive(Clone, Debug)]
 pub struct Neighbor {
     pub idx: usize,
     pub distance: f64,
@@ -398,7 +398,8 @@ fn max_spread_column(matrix: &ArrayView2<f64>, idx: &[usize]) -> usize {
 #[cfg(test)]
 mod test {
     use super::*;
-    use ndarray::{aview1, aview2};
+    use ndarray::{aview1, aview2, ArrayView, Axis};
+    use rand::prelude::*;
 
     #[test]
     #[should_panic]
@@ -493,6 +494,25 @@ mod test {
     }
 
     #[test]
+    fn ball_tree_query() {
+        const DIMENSION: usize = 3;
+
+        let mut rng = rand::thread_rng();
+        let data: Vec<f64> = (0..40 * DIMENSION).map(|_| rng.gen()).collect();
+        let array = ArrayView::from_shape((40, DIMENSION), &data).unwrap();
+        let bt = BallTree::new(array.clone());
+        for _ in 0..10 {
+            let query: Vec<f64> = (0..DIMENSION).map(|_| rng.gen()).collect();
+            let bt_neighbors = bt.query(&aview1(&query), 5);
+            let naive_neighbors =
+                naive_k_nearest_neighbors(&array, &aview1(&query), 5, euclidean_distance);
+            for (n_bt, n_naive) in bt_neighbors.iter().zip(naive_neighbors.iter()) {
+                assert_eq!(n_bt.distance, n_naive.distance);
+            }
+        }
+    }
+
+    #[test]
     fn node_init() {
         let distance = euclidean_distance;
         let data = [[0., 1.], [0., 9.], [0., 2.]];
@@ -573,5 +593,23 @@ mod test {
         let data = [[0., 1.], [0., 9.], [0., 2.]];
         let idx: [usize; 3] = [0, 1, 2];
         assert_eq!(super::max_spread_column(&aview2(&data), &idx), 1);
+    }
+
+    fn naive_k_nearest_neighbors<'a>(
+        neighbors: &ArrayView2<'a, f64>,
+        point: &ArrayView1<f64>,
+        k: usize,
+        distance: fn(&ArrayView1<f64>, &ArrayView1<f64>) -> f64,
+    ) -> Vec<Neighbor> {
+        let mut knn = neighbors
+            .axis_iter(Axis(0))
+            .enumerate()
+            .map(|(i, n)| Neighbor {
+                idx: i,
+                distance: distance(&n, point),
+            })
+            .collect::<Vec<Neighbor>>();
+        knn.sort();
+        knn[0..k].to_vec()
     }
 }
