78025288d40975f6ddc42186007383fc3767f2f0
Min Shao <mshao@petabi.com>
2021-06-22 22:11:06 -0700

    remove reduced distance

--- src/distance.rs
+++ src/distance.rs
@@ -7,7 +7,6 @@ use std::ops::AddAssign;
 /// The type of a distance metric function.
 pub trait Distance<A> {
     fn get(&self, _: &ArrayView1<A>, _: &ArrayView1<A>) -> A;
-    fn get_reduced(&self, _: &ArrayView1<A>, _: &ArrayView1<A>) -> A;
 }
 
 #[derive(Default)]
@@ -17,33 +16,8 @@ impl<A> Distance<A> for Euclidean
 where
     A: Float + Zero + AddAssign,
 {
-    /// Euclidean distance before taking a squre root. Used as a lightweight version
-    /// of [`euclidean`] for relative comparisions.
-    /// [`euclidean`]: #method.euclidean
-    fn get_reduced(&self, x1: &ArrayView1<A>, x2: &ArrayView1<A>) -> A {
-        x1.iter()
-            .zip(x2.iter())
-            .fold(A::zero(), |mut sum, (&v1, &v2)| {
-                let diff = v1 - v2;
-                sum += diff * diff;
-                sum
-            })
-    }
-
     /// Euclidean distance metric.
     fn get(&self, x1: &ArrayView1<A>, x2: &ArrayView1<A>) -> A {
-        self.get_reduced(x1, x2).sqrt()
-    }
-}
-
-#[derive(Default)]
-pub struct EuclideanNoReduced {}
-
-impl<A> Distance<A> for EuclideanNoReduced
-where
-    A: Float + Zero + AddAssign,
-{
-    fn get_reduced(&self, x1: &ArrayView1<A>, x2: &ArrayView1<A>) -> A {
         x1.iter()
             .zip(x2.iter())
             .fold(A::zero(), |mut sum, (&v1, &v2)| {
@@ -52,9 +26,5 @@ where
                 sum
             })
             .sqrt()
-    }
-
-    fn get(&self, x1: &ArrayView1<A>, x2: &ArrayView1<A>) -> A {
-        self.get_reduced(x1, x2)
     }
 }
